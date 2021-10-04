55651f4de43075c92f2a94db6983e25204a59ef8
Hideo Adachi <ckl15003@nitech.jp>
2021-10-04 22:26:10 +0900

    capture ethernet packet

--- ltest.c
+++ ltest.c
@@ -18,6 +18,9 @@ int InitRawSocket(char *device, int promiscFlag, int ipOnly)
 
     if (ipOnly)
     {
+        // First argument of socket() specified protocol family.
+        // If we use TCP or UDP, then we set PF_INET or PF_INET6.
+        //  For datalink layer, we specify PF_PACKET.
         if ((soc = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_IP))) < 0)
         {
             perror("socket");
@@ -83,3 +86,77 @@ int InitRawSocket(char *device, int promiscFlag, int ipOnly)
     return soc;
 }
 
+char *my_ether_ntoa_r(u_char *hwaddr, char *buf, socklen_t size)
+{
+    snprintf(buf, size, "%02x:%02x:%02x:%02x:%02x:%02x",
+             hwaddr[0], hwaddr[1], hwaddr[2], hwaddr[3], hwaddr[4], hwaddr[5]);
+
+    return buf;
+}
+
+int PrintEtherHeader(struct ether_header *eh, FILE *fp)
+{
+    char buf[80];
+    fprintf(fp, "ether_header----------------------------\n");
+    fprintf(fp, "ether_dhost=%s\n", my_ether_ntoa_r(eh->ether_dhost, buf, sizeof(buf)));
+    fprintf(fp, "ether_shost=%s\n", my_ether_ntoa_r(eh->ether_shost, buf, sizeof(buf)));
+    fprintf(fp, "ether_type=%02X", ntohs(eh->ether_type));
+    switch (ntohs(eh->ether_type))
+    {
+    case ETH_P_IP:
+        fprintf(fp, "(IP)\n");
+        break;
+    case ETH_P_IPV6:
+        fprintf(fp, "(IPv6)\n");
+        break;
+    case ETH_P_ARP:
+        fprintf(fp, "(ARP)\n");
+        break;
+    default:
+        fprintf(fp, "(unknown)\n");
+        break;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+    int soc, size;
+    u_char buf[2048];
+
+    if (argc <= 1)
+    {
+        fprintf(stderr, "ltest device-name\n");
+        return (1);
+    }
+
+    if ((soc = InitRawSocket(argv[1], 0, 0)) == -1)
+    {
+        fprintf(stderr, "InitRawSocket:error:%s¥n", argv[1]);
+        return -1;
+    }
+
+    while (1)
+    {
+        if ((size = read(soc, buf, sizeof(buf))) <= 0)
+        {
+            perror("read");
+        }
+        else
+        {
+            if (size >= sizeof(struct ether_header))
+            {
+                PrintEtherHeader((struct ether_header *)buf, stdout);
+            }
+            else
+            {
+                fprintf(stderr, "read size(%d) < %d¥n", size, sizeof(struct ether_header));
+            }
+        }
+    }
+
+    close(soc);
+
+    return 0;
+}
